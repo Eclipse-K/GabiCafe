6789e121fbb7e61d6f43e44dc2b8022eace4304d
Eclipse_K <godo8522@naver.com>
2023-09-05 15:12:49 +0900

    Add Products

---
+++ src/Components/Products.js
@@ -0,0 +1,293 @@
+import Dripper from "../Images/Dripper.png";
+import CoffeeServer from "../Images/Coffee_Server.png";
+import EspressoMachine from "../Images/Espresso_Machine.png";
+import EspressoMaker from "../Images/Espresso_Maker.png";
+import FrenchPress from "../Images/French_Press.png";
+import MocaPot from "../Images/Moca_pot.png";
+import DutchMaker from "../Images/Dutch_Maker.png";
+import CoffeeGrinder from "../Images/Coffee_Grinder.png";
+import Siphon from "../Images/Siphon.png";
+import CoffeeCapsuleMachine from "../Images/Coffee_Capsule_Machine.png";
+import MilkFrother from "../Images/Milk_Frother.png";
+import SteamingPitcher from "../Images/Steaming_Pitcher.png";
+import BellCreamer from "../Images/Bell_Creamer.png";
+import DripPot from "../Images/Drip_Pot.png";
+import PaperCoffeeFilter from "../Images/Paper_Coffee_Filter.png";
+import CoffeeTamper from "../Images/Coffee_Tamper.png";
+import CoffeeDistributor from "../Images/Coffee_Distributor.png";
+import VietnamesePhinDripper from "../Images/Vietnamese_Phin_Dripper.png";
+import CoffeeCaddies from "../Images/Coffee_Caddies.png";
+
+function Products() {
+  return (
+    <div class="menus">
+      <h3>- Products -</h3>
+
+      <div class="product_list">
+        <ul>
+          <li
+            data-aos="flip-left"
+            data-aos-easing="ease-out-cubic"
+            data-aos-duration="1000"
+          >
+            <div class="img">
+              <img src={Dripper} alt="드리퍼" width="400px" height="300px" />
+            </div>
+          </li>
+
+          <li
+            data-aos="flip-left"
+            data-aos-easing="ease-out-cubic"
+            data-aos-duration="1000"
+          >
+            <div class="img">
+              <img
+                src={CoffeeServer}
+                alt="커피서버"
+                width="400px"
+                height="300px"
+              />
+            </div>
+          </li>
+
+          <li
+            data-aos="flip-left"
+            data-aos-easing="ease-out-cubic"
+            data-aos-duration="1000"
+          >
+            <div class="img">
+              <img
+                src={EspressoMachine}
+                alt="반자동에스프레소머신"
+                width="500px"
+                height="300px"
+              />
+            </div>
+          </li>
+
+          <li
+            data-aos="flip-left"
+            data-aos-easing="ease-out-cubic"
+            data-aos-duration="1000"
+          >
+            <div class="img">
+              <img
+                src={EspressoMaker}
+                alt="수동에스프레소머신"
+                width="600px"
+                height="400px"
+              />
+            </div>
+          </li>
+
+          <li
+            data-aos="flip-left"
+            data-aos-easing="ease-out-cubic"
+            data-aos-duration="1000"
+          >
+            <div class="img">
+              <img
+                src={FrenchPress}
+                alt="프렌치프레스"
+                width="400px"
+                height="400px"
+              />
+            </div>
+          </li>
+
+          <li
+            data-aos="flip-left"
+            data-aos-easing="ease-out-cubic"
+            data-aos-duration="1000"
+          >
+            <div class="img">
+              <img src={MocaPot} alt="모카포트" width="300px" height="400px" />
+            </div>
+          </li>
+
+          <li
+            data-aos="flip-left"
+            data-aos-easing="ease-out-cubic"
+            data-aos-duration="1000"
+          >
+            <div class="img">
+              <img
+                src={DutchMaker}
+                alt="더치메이커"
+                width="300px"
+                height="400px"
+              />
+            </div>
+          </li>
+
+          <li
+            data-aos="flip-left"
+            data-aos-easing="ease-out-cubic"
+            data-aos-duration="1000"
+          >
+            <div class="img">
+              <img
+                src={CoffeeGrinder}
+                alt="커피그라인더"
+                width="350px"
+                height="400px"
+              />
+            </div>
+          </li>
+
+          <li
+            data-aos="flip-left"
+            data-aos-easing="ease-out-cubic"
+            data-aos-duration="1000"
+          >
+            <div class="img">
+              <img src={Siphon} alt="사이폰" width="300px" height="400px" />
+            </div>
+          </li>
+
+          <li
+            data-aos="flip-left"
+            data-aos-easing="ease-out-cubic"
+            data-aos-duration="1000"
+          >
+            <div class="img">
+              <img
+                src={CoffeeCapsuleMachine}
+                alt="캡슐머신"
+                width="350px"
+                height="195px"
+              />
+            </div>
+          </li>
+
+          <li
+            data-aos="flip-left"
+            data-aos-easing="ease-out-cubic"
+            data-aos-duration="1000"
+          >
+            <div class="img">
+              <img
+                src={MilkFrother}
+                alt="우유거품기"
+                width="350px"
+                height="200px"
+              />
+            </div>
+          </li>
+
+          <li
+            data-aos="flip-left"
+            data-aos-easing="ease-out-cubic"
+            data-aos-duration="1000"
+          >
+            <div class="img">
+              <img
+                src={SteamingPitcher}
+                alt="스팀피처"
+                width="350px"
+                height="300px"
+              />
+            </div>
+          </li>
+
+          <li
+            data-aos="flip-left"
+            data-aos-easing="ease-out-cubic"
+            data-aos-duration="1000"
+          >
+            <div class="img">
+              <img
+                src={BellCreamer}
+                alt="벨크리머"
+                width="300px"
+                height="300px"
+              />
+            </div>
+          </li>
+
+          <li
+            data-aos="flip-left"
+            data-aos-easing="ease-out-cubic"
+            data-aos-duration="1000"
+          >
+            <div class="img">
+              <img src={DripPot} alt="드립포트" width="300px" height="300px" />
+            </div>
+          </li>
+
+          <li
+            data-aos="flip-left"
+            data-aos-easing="ease-out-cubic"
+            data-aos-duration="1000"
+          >
+            <div class="img">
+              <img
+                src={PaperCoffeeFilter}
+                alt="종이커피필터"
+                width="350px"
+                height="300px"
+              />
+            </div>
+          </li>
+
+          <li
+            data-aos="flip-left"
+            data-aos-easing="ease-out-cubic"
+            data-aos-duration="1000"
+          >
+            <div class="img">
+              <img src={CoffeeTamper} alt="탬퍼" width="300px" height="250px" />
+            </div>
+          </li>
+
+          <li
+            data-aos="flip-left"
+            data-aos-easing="ease-out-cubic"
+            data-aos-duration="1000"
+          >
+            <div class="img">
+              <img
+                src={CoffeeDistributor}
+                alt="디스트리뷰터"
+                width="300px"
+                height="250px"
+              />
+            </div>
+          </li>
+
+          <li
+            data-aos="flip-left"
+            data-aos-easing="ease-out-cubic"
+            data-aos-duration="1000"
+          >
+            <div class="img">
+              <img
+                src={VietnamesePhinDripper}
+                alt="베트남식 드리퍼"
+                width="300px"
+                height="250px"
+              />
+            </div>
+          </li>
+
+          <li
+            data-aos="flip-left"
+            data-aos-easing="ease-out-cubic"
+            data-aos-duration="1000"
+          >
+            <div class="img">
+              <img
+                src={CoffeeCaddies}
+                alt="커피통"
+                width="300px"
+                height="250px"
+              />
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Products;
