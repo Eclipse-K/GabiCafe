4179fec56d9746e13c6e9d5c5eb3dbcb17a7e42e
Eclipse_K <godo8522@naver.com>
2023-09-07 15:01:11 +0900

    Edit Products.css

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -30,7 +30,7 @@ function Products() {
       <div className="menus">
         <h3>- Products -</h3>
 
-        <div className="product_list">
+        <div className="Products-list">
           <div
             className="Products-box"
             data-aos="fade-left"
@@ -39,6 +39,7 @@ function Products() {
             data-aos-duration="1400"
           >
             <img className="Products-img" src={Dripper} alt="드리퍼" />
+            <p className="Products-content">드리퍼</p>
           </div>
 
           <div
@@ -76,210 +77,174 @@ function Products() {
             />
           </div>
 
-          <li
+          <div
+            className="Products-box"
             data-aos="fade-up"
             data-aos-easing="linear"
             data-aos-duration="1500"
           >
-            <div className="img">
-              <img
-                src={FrenchPress}
-                alt="프렌치프레스"
-                width="400px"
-                height="400px"
-              />
-            </div>
-          </li>
+            <img
+              className="Products-img"
+              src={FrenchPress}
+              alt="프렌치프레스"
+            />
+          </div>
 
-          <li
+          <div
+            className="Products-box"
             data-aos="fade-up"
             data-aos-easing="linear"
             data-aos-duration="1500"
           >
-            <div className="img">
-              <img src={MocaPot} alt="모카포트" width="300px" height="400px" />
-            </div>
-          </li>
+            <img className="Products-img" src={MocaPot} alt="모카포트" />
+          </div>
 
-          <li
+          <div
+            className="Products-box"
             data-aos="fade-up"
             data-aos-easing="linear"
             data-aos-duration="1500"
           >
-            <div className="img">
-              <img
-                src={DutchMaker}
-                alt="더치메이커"
-                width="300px"
-                height="400px"
-              />
-            </div>
-          </li>
+            <img className="Products-img" src={DutchMaker} alt="더치메이커" />
+          </div>
 
-          <li
+          <div
+            className="Products-box"
             data-aos="fade-up"
             data-aos-easing="linear"
             data-aos-duration="1500"
           >
-            <div className="img">
-              <img
-                src={CoffeeGrinder}
-                alt="커피그라인더"
-                width="350px"
-                height="400px"
-              />
-            </div>
-          </li>
+            <img
+              className="Products-img"
+              src={CoffeeGrinder}
+              alt="커피그라인더"
+            />
+          </div>
 
-          <li
+          <div
+            className="Products-box"
             data-aos="fade-up"
             data-aos-easing="linear"
             data-aos-duration="1500"
           >
-            <div className="img">
-              <img src={Siphon} alt="사이폰" width="300px" height="400px" />
-            </div>
-          </li>
+            <img className="Products-img" src={Siphon} alt="사이폰" />
+          </div>
 
-          <li
+          <div
+            className="Products-box"
             data-aos="fade-up"
             data-aos-easing="linear"
             data-aos-duration="1500"
           >
-            <div className="img">
-              <img
-                src={CoffeeCapsuleMachine}
-                alt="캡슐머신"
-                width="350px"
-                height="195px"
-              />
-            </div>
-          </li>
+            <img
+              className="Products-img"
+              src={CoffeeCapsuleMachine}
+              alt="캡슐머신"
+            />
+          </div>
 
-          <li
+          <div
+            className="Products-box"
             data-aos="fade-up"
             data-aos-easing="linear"
             data-aos-duration="1500"
           >
-            <div className="img">
-              <img
-                src={MilkFrother}
-                alt="우유거품기"
-                width="350px"
-                height="200px"
-              />
-            </div>
-          </li>
+            <img className="Products-img" src={MilkFrother} alt="우유거품기" />
+          </div>
 
-          <li
+          <div
+            className="Products-box"
             data-aos="fade-up"
             data-aos-easing="linear"
             data-aos-duration="1500"
           >
-            <div className="img">
-              <img
-                src={SteamingPitcher}
-                alt="스팀피처"
-                width="350px"
-                height="300px"
-              />
-            </div>
-          </li>
+            <img
+              className="Products-img"
+              src={SteamingPitcher}
+              alt="스팀피처"
+            />
+          </div>
 
-          <li
+          <div
+            className="Products-box"
             data-aos="fade-up"
             data-aos-easing="linear"
             data-aos-duration="1500"
           >
-            <div className="img">
-              <img
-                src={BellCreamer}
-                alt="벨크리머"
-                width="300px"
-                height="300px"
-              />
-            </div>
-          </li>
+            <img className="Products-img" src={BellCreamer} alt="벨크리머" />
+          </div>
 
-          <li
+          <div
+            className="Products-box"
             data-aos="fade-up"
             data-aos-easing="linear"
             data-aos-duration="1500"
           >
-            <div className="img">
-              <img src={DripPot} alt="드립포트" width="300px" height="300px" />
-            </div>
-          </li>
+            <img className="Products-img" src={DripPot} alt="드립포트" />
+          </div>
 
-          <li
+          <div
+            className="Products-box"
             data-aos="fade-up"
             data-aos-easing="linear"
             data-aos-duration="1500"
           >
-            <div className="img">
-              <img
-                src={PaperCoffeeFilter}
-                alt="종이커피필터"
-                width="350px"
-                height="300px"
-              />
-            </div>
-          </li>
+            <img
+              className="Products-img"
+              src={PaperCoffeeFilter}
+              alt="종이커피필터"
+            />
+          </div>
 
-          <li
+          <div
+            className="Products-box"
             data-aos="fade-up"
             data-aos-easing="linear"
             data-aos-duration="1500"
           >
-            <div className="img">
-              <img src={CoffeeTamper} alt="탬퍼" width="300px" height="250px" />
-            </div>
-          </li>
+            <img
+              className="Products-img"
+              src={CoffeeTamper}
+              alt="탬퍼"
+              width="300px"
+              height="250px"
+            />
+          </div>
 
-          <li
+          <div
+            className="Products-box"
             data-aos="fade-up"
             data-aos-easing="linear"
             data-aos-duration="1500"
           >
-            <div className="img">
-              <img
-                src={CoffeeDistributor}
-                alt="디스트리뷰터"
-                width="300px"
-                height="250px"
-              />
-            </div>
-          </li>
+            <img
+              className="Products-img"
+              src={CoffeeDistributor}
+              alt="디스트리뷰터"
+            />
+          </div>
 
-          <li
+          <div
+            className="Products-box"
             data-aos="fade-up"
             data-aos-easing="linear"
             data-aos-duration="1500"
           >
-            <div className="img">
-              <img
-                src={VietnamesePhinDripper}
-                alt="베트남식 드리퍼"
-                width="300px"
-                height="250px"
-              />
-            </div>
-          </li>
+            <img
+              className="Products-img"
+              src={VietnamesePhinDripper}
+              alt="베트남식 드리퍼"
+            />
+          </div>
 
-          <li
+          <div
+            className="Products-box"
             data-aos="fade-up"
             data-aos-easing="linear"
             data-aos-duration="1500"
           >
-            <div className="img">
-              <img
-                src={CoffeeCaddies}
-                alt="커피통"
-                width="300px"
-                height="250px"
-              />
-            </div>
-          </li>
+            <img className="Products-img" src={CoffeeCaddies} alt="커피통" />
+          </div>
         </div>
       </div>
     </div>
