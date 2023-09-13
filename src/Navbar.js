cfc6267bbb5802aee640a6eb53fe8df93cc3b826
Eclipse_K <godo8522@naver.com>
2023-09-13 15:58:14 +0900

    Add system Theme

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,6 +4,19 @@ import { FiAlignJustify, FiX } from "react-icons/fi";
 import "./Navbar.css";
 import { useState } from "react";
 import OffcanvasMenu from "./OffcanvasMenu";
+import NavContainer from "./NavbarElement/NavContainer";
+import useSystemTheme from "./Components/useSystemTheme";
+import { ThemeProvider } from "styled-components";
+
+const LightMode = {
+  backgroundColor: "white",
+  textColor: "black",
+};
+
+const DarkMode = {
+  backgroundColor: "black",
+  textColor: "white",
+};
 
 function Navbar() {
   const [isCollapsed, setIsCollapsed] = useState(false);
@@ -16,24 +29,32 @@ function Navbar() {
     setIsCollapsed(false);
   };
 
+  //테마로 색상 변경
+  const systemTheme = useSystemTheme();
+
   return (
-    <nav className="navbar-offcanvas-container">
-      <Link className="navbar-logo" to="/">
-        <img src={Logo} alt={Logo} />
-      </Link>
-      <div className={`navbar-offcanvas ${isCollapsed ? "open" : ""}`}>
-        {isCollapsed ? (
-          <button className="offcanvas-button rotated" onClick={closeOffcanvas}>
-            <FiX />
-          </button>
-        ) : (
-          <button className="offcanvas-button" onClick={toggleCollapse}>
-            <FiAlignJustify />
-          </button>
-        )}
-      </div>
-      {isCollapsed && <OffcanvasMenu closeOffcanvas={closeOffcanvas} />}
-    </nav>
+    <ThemeProvider theme={systemTheme === "dark" ? DarkMode : LightMode}>
+      <NavContainer>
+        <Link className="navbar-logo" to="/">
+          <img src={Logo} alt={Logo} />
+        </Link>
+        <div className={`navbar-offcanvas ${isCollapsed ? "open" : ""}`}>
+          {isCollapsed ? (
+            <button
+              className="offcanvas-button rotated"
+              onClick={closeOffcanvas}
+            >
+              <FiX />
+            </button>
+          ) : (
+            <button className="offcanvas-button" onClick={toggleCollapse}>
+              <FiAlignJustify />
+            </button>
+          )}
+        </div>
+        {isCollapsed && <OffcanvasMenu closeOffcanvas={closeOffcanvas} />}
+      </NavContainer>
+    </ThemeProvider>
   );
 }
 
---
+++ src/NavbarElement/NavContainer.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const NavContainer = styled.nav`
+  display: flex;
+  position: relative;
+  justify-content: space-between;
+  align-items: center;
+  background-color: white;
+  padding: 10px;
+  box-shadow: 4px 0 5px gray;
+  border-bottom: 1px groove gray;
+  z-index: 3;
+`;
+
+export default NavContainer;
