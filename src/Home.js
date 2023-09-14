c5629970bc01f75b9aaf7a8f6640f1dc5a2ae28b
Eclipse_K <godo8522@naver.com>
2023-09-14 15:18:42 +0900

    Edit Home img Slide

--- src/Home.js
+++ src/Home.js
@@ -1,48 +1,48 @@
+import React, { useState, useEffect } from "react";
 import "./Home.css";
 import Navbar from "./Navbar";
+import Gyeongbokgung from "./Images/Gyeongbokgung_Palace.jpg";
+import GyeonghoeruPavilion from "./Images/GyeonghoeruPavilion.png";
+import Gwanghwamun from "./Images/Gwanghwamun.png";
+import BuyongjeongPavilion from "./Images/BuyongjeongPavilion.png";
+import Seokjojeon from "./Images/Deoksugung_Seokjojeon.jpeg";
+import Jeonggwanheon from "./Images/DeoksugungJeonggwanheon.jpeg";
 
 function Home() {
+  const images = [
+    Gyeongbokgung,
+    GyeonghoeruPavilion,
+    Gwanghwamun,
+    BuyongjeongPavilion,
+    Seokjojeon,
+    Jeonggwanheon,
+  ];
+  const imageCount = images.length;
+
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      // 다음 이미지로 이동
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % imageCount);
+    }, 5000); // 3초마다 이미지 변경
+
+    return () => {
+      // 컴포넌트가 unmount 되면 타이머를 정리합니다.
+      clearInterval(intervalId);
+    };
+  }, [imageCount]);
+
   return (
     <div>
       <Navbar />
 
-      <div
-        id="carouselExampleFade"
-        className="carousel slide carousel-fade"
-        data-bs-ride="carousel"
-      >
-        <div className="carousel-inner">
-          <div className="carousel-item item1 active d-block w-100"></div>
-          <div className="carousel-item item2 d-block w-100"></div>
-          <div className="carousel-item item3 d-block w-100"></div>
-          <div className="carousel-item item4 d-block w-100"></div>
-          <div className="carousel-item item5 d-block w-100"></div>
-          <div className="carousel-item item6 d-block w-100"></div>
-        </div>
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleFade"
-          data-bs-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleFade"
-          data-bs-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Next</span>
-        </button>
+      <div className="Home">
+        <img
+          className="Slide-img"
+          src={images[currentImageIndex]}
+          alt={`Cat ${currentImageIndex + 1}`}
+        />
       </div>
     </div>
   );
