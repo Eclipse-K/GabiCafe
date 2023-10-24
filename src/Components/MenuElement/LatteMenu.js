8a91395815d6437b589f4dabbb67913b65c91b23
Eclipse_K <godo8522@naver.com>
2023-10-24 13:50:13 +0900

    Add LatteMenu in ReverseButton

--- src/Components/MenuElement/LatteMenu.js
+++ src/Components/MenuElement/LatteMenu.js
@@ -3,6 +3,9 @@ import Navbar from "../../Navbar";
 import LatteJson from "./CafeMenu.json";
 import CafeMenuContainer from "./C-Menu-Element/CafeMenuContainer";
 import CafeMenuImgItem from "./C-Menu-Element/CafeMenuImgItem";
+import ReverseButton from "./C-Menu-Element/ReverseButton";
+import { CiUndo } from "react-icons/ci";
+import { Link } from "react-router-dom";
 
 function LatteMenu() {
   // eslint-disable-next-line
@@ -24,6 +27,11 @@ function LatteMenu() {
           </CafeMenuImgItem>
         ))}
       </CafeMenuContainer>
+      <ReverseButton>
+        <Link className="Reverse-link" to="/Menu">
+          <CiUndo className="Reverse-icon" />
+        </Link>
+      </ReverseButton>
     </div>
   );
 }
