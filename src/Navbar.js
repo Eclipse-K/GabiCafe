0505ad981fee6ccc950e67c3c0d3b48d79925169
Eclipse_K <godo8522@naver.com>
2023-11-05 15:02:35 +0900

    Add isMobile useState

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import Logo from "./Components/Images/LogoGabi.png";
 import { FiX } from "react-icons/fi";
 import "./Navbar.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import OffcanvasMenu from "./OffcanvasMenu";
 import NavContainer from "./Components/NavbarElement/NavContainer";
 import useSystemTheme from "./Components/useSystemTheme";
@@ -21,6 +21,7 @@ const DarkMode = {
 function Navbar() {
   const [isCollapsed, setIsCollapsed] = useState(false);
   const [isHovered, setIsHovered] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
 
   const toggleCollapse = () => {
     setIsCollapsed((prevCollapsed) => !prevCollapsed);
@@ -34,33 +35,57 @@ function Navbar() {
   const systemTheme = useSystemTheme();
   console.log(systemTheme);
 
+  //이벤트 리스너를 통해 화면 크기 변경을 감지
+  useEffect(() => {
+    function handleResize() {
+      setIsMobile(window.innerWidth <= 768); //아이패드 미니 크기
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <ThemeProvider theme={systemTheme === "dark" ? DarkMode : LightMode}>
       <NavContainer>
         <Link className="navbar-logo" to="/">
           <img src={Logo} alt={Logo} />
         </Link>
-        <div className={`navbar-offcanvas ${isCollapsed ? "open" : ""}`}>
-          {isCollapsed ? (
-            <button
-              className="offcanvas-button rotated"
-              onClick={closeOffcanvas}
-            >
-              <FiX />
-            </button>
+        <div>
+          {isMobile ? (
+            //화면이 작을 때, 아이콘이 나타남
+            <div className={`navbar-offcanvas ${isCollapsed ? "open" : ""}`}>
+              {isCollapsed ? (
+                <button
+                  className="offcanvas-button rotated"
+                  onClick={closeOffcanvas}
+                >
+                  <FiX />
+                </button>
+              ) : (
+                <button
+                  className={`offcanvas-button ${isHovered ? "hovered" : ""}`}
+                  onClick={toggleCollapse}
+                  onMouseEnter={() => setIsHovered(true)}
+                  onMouseLeave={() => setIsHovered(false)}
+                >
+                  <span className={`line ${isHovered ? "hovered" : ""}`}></span>
+                  <span className={`line ${isHovered ? "hovered" : ""}`}></span>
+                  <span className={`line ${isHovered ? "hovered" : ""}`}></span>
+                </button>
+              )}
+            </div>
           ) : (
-            <button
-              className={`offcanvas-button ${isHovered ? "hovered" : ""}`}
-              onClick={toggleCollapse}
-              onMouseEnter={() => setIsHovered(true)}
-              onMouseLeave={() => setIsHovered(false)}
-            >
-              <span className={`line ${isHovered ? "hovered" : ""}`}></span>
-              <span className={`line ${isHovered ? "hovered" : ""}`}></span>
-              <span className={`line ${isHovered ? "hovered" : ""}`}></span>
-            </button>
+            //화면이 큰 경우, 모든 탭 표시
+            <>
+              <div>아이</div>
+            </>
           )}
         </div>
+
         {isCollapsed && <OffcanvasMenu closeOffcanvas={closeOffcanvas} />}
       </NavContainer>
     </ThemeProvider>
