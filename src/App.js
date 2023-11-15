9d5e82a13e7777cf6fecb30879628d14a0dcd49b
Eclipse_K <godo8522@naver.com>
2023-11-15 22:48:07 +0900

    Edit ModeChange

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Home from "./Home";
 import Menu from "./Components/Menu";
 import Products from "./Components/Products";
 import Login from "./Components/Login";
@@ -12,23 +11,16 @@ import Tea from "./Components/MenuElement/Tea";
 import Smoothie from "./Components/MenuElement/Smoothie";
 import Copyright from "./Components/Copyright";
 import Information from "./Components/Information";
-
-import styled from "styled-components";
+import { useState, useEffect } from "react";
 import ReactSwitch from "react-switch";
-import { useEffect, useState } from "react";
-
-const Mode = styled.div`
-  background-color: ${(props) =>
-    props.isDarkMode ? "rgb(35, 35, 35)" : "#C2C2B1"};
-  color: ${(props) => (props.isDarkMode ? "white" : "black")};
-`;
+import Home from "./Home";
 
 function App() {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [modeChange, setModeChange] = useState(false);
 
   // 모드 변경 함수
-  const toggleDarkMode = () => {
-    setIsDarkMode(!isDarkMode);
+  const toggleModeChange = () => {
+    setModeChange(!modeChange);
   };
 
   // 사용자의 환경 설정에 따라 모드를 자동으로 변경
@@ -36,12 +28,12 @@ function App() {
     const prefersDarkMode = window.matchMedia(
       "(prefers-color-scheme: dark)"
     ).matches;
-    setIsDarkMode(prefersDarkMode);
+    setModeChange(prefersDarkMode);
   }, []);
 
   return (
     <div className="App">
-      <Mode isDarkMode={isDarkMode}>
+      <div className={modeChange ? "dark-mode" : "light-mode"}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Information" element={<Information />} />
@@ -55,10 +47,9 @@ function App() {
           <Route path="/Tea" element={<Tea />} />
           <Route path="/Smoothie" element={<Smoothie />} />
         </Routes>
-        <ReactSwitch onChange={toggleDarkMode} checked={isDarkMode} />
-
+        <ReactSwitch onChange={toggleModeChange} checked={modeChange} />
         <Copyright />
-      </Mode>
+      </div>
     </div>
   );
 }
