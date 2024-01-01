1d299ebc405ecf849bfad21f9b54f43257201993
Eclipse_K <godo8522@naver.com>
2024-01-01 23:16:00 +0900

    Add Top: Large image and Bottom: Small image in Products

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -8,6 +8,20 @@ import ProJson from "./Products.json";
 function Products() {
   // eslint-disable-next-line
   const [products, setProducts] = useState(ProJson.products);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleImageClick = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % products.length);
+  };
+
+  const handleProductImage = (index) => {
+    if (products[index]) {
+      setCurrentIndex(index);
+    }
+  };
+
+  // 현재 선택된 제품 정보 가져오기
+  const currentProduct = products[currentIndex];
 
   return (
     <div>
@@ -16,12 +30,22 @@ function Products() {
       <ProductsContainer>
         <ProductsList>
           <h1>- Products -</h1>
+          <img
+            src={
+              process.env.PUBLIC_URL + `/images/${currentProduct.productUrl}`
+            }
+            alt={currentProduct.product_name}
+            className="mainImage"
+            onClick={handleImageClick}
+          />
+
           <div>
             {products.map((product, index) => (
               <ProductsBox key={index}>
                 <img
                   src={process.env.PUBLIC_URL + `/images/${product.productUrl}`}
                   alt={product.product_name}
+                  onClick={() => handleProductImage(index)}
                 />
                 <p>{product.product_name}</p>
               </ProductsBox>
